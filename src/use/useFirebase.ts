9302f32a95216ac8f518dafac06b356237e85790
Myziura <myziura.work@gmail.comgit config --global user.name Myziura>
2022-05-20 21:09:07 +0300

    Remove private data

--- src/use/useFirebase.ts
+++ src/use/useFirebase.ts
@@ -2,17 +2,7 @@ import { initializeApp } from 'firebase/app'
 
 import generateApi from '@/api'
 
-const app = initializeApp({
-  apiKey: 'AIzaSyAhGhkLSOcNf9YrysHVgV7oDnhbhmmP1sk',
-  authDomain: 'is-it-russian.firebaseapp.com',
-  databaseURL:
-    'https://is-it-russian-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'is-it-russian',
-  storageBucket: 'is-it-russian.appspot.com',
-  messagingSenderId: '704575387505',
-  appId: '1:704575387505:web:062cf53583cebebcfce8d0',
-  measurementId: 'G-EYTBX786DV'
-})
+const app = initializeApp({})
 
 const api = generateApi(app)
 
