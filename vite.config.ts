9bfa5215f5c5fee75ac23eab799685e2f09f420a
Михаил Мизюра <myziura.work@gmail.com>
2022-07-30 09:39:06 +0300

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 
 export default defineConfig({
+  base: import.meta.env.PROD ? '/is-it-russian/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
