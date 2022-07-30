1dc4df3d3b887c5c4b285bf5b6a15e4b5476eebe
Михаил Мизюра <myziura.work@gmail.com>
2022-07-30 09:52:24 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 
-export default defineConfig({
-  base: import.meta.env.PROD ? '/is-it-russian/' : '/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default () => {
+  console.log(process.env)
+
+  return defineConfig({
+    // base: process.env.PROD ? '/is-it-russian/' : '',
+    base: '/is-it-russian/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     }
-  }
-})
+  })
+}
